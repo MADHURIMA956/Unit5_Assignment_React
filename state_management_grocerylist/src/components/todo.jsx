c887868904f32cf0734b81a1b19d78e99edde908
Madhurima Banerjee <91532700+MADHURIMA956@users.noreply.github.com>
2021-12-29 20:17:03 +0530

    commit

---
+++ state_management_grocerylist/src/components/todo.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+export const Todo = () => {
+    return (
+        <>
+            <div className='man_div'>
+                <div className='child_div'>
+                    <figure>
+                        <img src="https://media.istockphoto.com/photos/groceries-picture-id171302954?k=20&m=171302954&s=612x612&w=0&h=kDpFXA8IoG22Uxog3YUmWvzBRzwyibd6r4S2v5z-Mno=" alt="grocery image" />
+                    </figure>
+                </div>
+            </div>
+        </>
+    )
+}
